28ecf28e571c42e98baa8f322e3f1e52f3007aa8
Theo-X <57161038+Theo-X@users.noreply.github.com>
2019-11-24 17:20:12 -0600

    Update Voltage_alarm_board_2.0.ino

--- Voltage_alarm_board/Voltage_alarm_board_2.0/Voltage_alarm_board_2.0.ino
+++ Voltage_alarm_board/Voltage_alarm_board_2.0/Voltage_alarm_board_2.0.ino
@@ -1,12 +1,17 @@
-int buz = 6; // buzzer pin
-int voltage = 0; // battery voltage
-int GL = 10; // green LED
-int YL = 9; // yellow LED
-int RL = 8; // red LED
-int mid = 0; // etra integer to caluclate voltage
-int incvoltage = 0; // etra integer to caluclate voltage "inc" stands for incoming. member that the incoming voltage has been divide by 4.
+int buz = 6; // Buzzer pin
+float voltage = 0; // Battery voltage
+int GL = 10; // Green LED
+int YL = 9; // Yellow LED
+int RL = 8; // Red LED
+float mid = 0; // Extra integer to caluclate voltage
+float incvoltage = 0; // Extra integer to caluclate voltage "inc" stands for incoming. Remember that the incoming voltage has been divided by 4.
+# include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 void setup()
 {
+  lcd.begin(16, 2);
+  lcd.clear();
   Serial.begin(9600);
   pinMode(buz, OUTPUT);
   pinMode(GL, OUTPUT);
@@ -17,24 +22,43 @@ void setup()
 
 void loop()
 {
+  lcd.clear();
   mid = analogRead(A0);
   incvoltage = mid / 206.4;
   voltage = incvoltage * 4;
   Serial.print(voltage);
-  
-  
+  lcd.setCursor(0, 1);
+  lcd.print(" STATUS:");
+  lcd.setCursor(0, 0);
+  lcd.print("VOLTAGE:       V");
+  if(voltage >= 10)
+  {
+    lcd.setCursor(9, 0);
+    lcd.print(voltage);
+  }
+  else if(voltage < 10)
+  {
+    lcd.setCursor(10, 0);
+    lcd.print(voltage);
+  }
   
   if ( voltage >= 8)
   {
     digitalWrite(YL, LOW);
     digitalWrite(RL, LOW);
     digitalWrite(GL, HIGH);
+    lcd.setCursor(12, 1);
+    lcd.print("GOOD");
+    delay(5);
   }
   else if (voltage >= 6)
   {
     digitalWrite(GL, LOW);
     digitalWrite(RL, LOW);
     digitalWrite(YL, HIGH);
+    lcd.setCursor(9, 1);
+    lcd.print("WARNING");
+    delay(5);
   }
   else if (voltage < 6)
   {
@@ -45,8 +69,11 @@ void loop()
     digitalWrite(RL, LOW);
     if (voltage < 6)
     {
-      loop();
+      lcd.setCursor(9, 1);
+      lcd.print("REPLACE");
+      if(voltage < 6)
       {
+        for (int i = 0; i < 100,000,000; i++)
         digitalWrite(RL, HIGH);
         for (int i = 0; i < 500; i++)
         {
